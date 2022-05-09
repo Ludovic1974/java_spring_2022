56d4928f128c31c0c2fea90c44d696d4d838f59f
Usuario <Ludovic1974>
2022-05-09 16:29:50 +0200

    Una última prueba de subida desde Eclipse

---
+++ ProyectoGitHub/src/github/MainGitHub.java
@@ -0,0 +1,16 @@
+package github;
+
+public class MainGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Prueba de impresión en pantalla");
+		System.out.println("Otro cambio");
+
+		System.out.println("Nuevo cambio sobre el proyecto");
+		User ludo = new User("Ludo", "ludo.efecto.mariposa@gmail.com");
+		System.out.println(ludo);
+		System.out.println("Último cambio de verdad");
+	}
+
+}
