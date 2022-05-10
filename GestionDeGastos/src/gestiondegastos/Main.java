c0a53de467b83c3e64538ea472ca7309854dbcef
Usuario <Ludovic1974>
2022-05-10 13:46:02 +0200

    cambio

---
+++ GestionDeGastos/src/gestiondegastos/Main.java
@@ -0,0 +1,14 @@
+package gestiondegastos;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		Menu.crearListaPrevia();
+		System.out.printf("Cambio");
+		System.out.printf("Cambio2");
+		
+		new Menu(); 
+
+	}
+
+}
